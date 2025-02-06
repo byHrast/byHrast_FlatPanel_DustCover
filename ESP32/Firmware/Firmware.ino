7dcebe2b564c6f0c72189a61244a5702cda4dde3
Matija Pozojevic <41533472+byHrast@users.noreply.github.com>
2025-02-06 09:48:11 +0100

    Add files via upload
    Some BUG fixes;
    1) ServoESP32 libary uses PPM so it messes up PWM of ledENA, so I added; analogWriteFrequency(ledENA, 1000);
    2) Changed the way to read a state of the ledENA from digitalRead to analogRead (as the ledENA is PWM)

--- ESP32/Firmware/Firmware.ino
+++ ESP32/Firmware/Firmware.ino
@@ -2,14 +2,23 @@
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 #include <ESP32Servo.h>
+ESP32PWM pwm;
 
 // Defaultne WiFi postavke
 const char* ssid     = "FlatPanel";
 const char* password = "123456789";
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 112);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
+const char* hostname = "FlatPanelByHrast";
 
 //Servo varijable
 Servo myservo;  // create servo object to control a servo
-int pos = 0;  // variable to store the servo position
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 int servoPin = 13;
 int servoENA = 32;
@@ -17,6 +26,7 @@ int servoENA = 32;
 // Definiranje IO
 const int ledPWM = 0;
 const int ledENA = 26;
+int ledMax = 100;
 const int heaterENA = 27;
 
 int thermistorPin1 = 25;   // Thermistor u panelu
@@ -25,9 +35,10 @@ int thermistorPin2 = 15;   // Thermistor na plocici
 int thermistorValue2 = 0;
 
 //Pomocne varijable
-String ledState;
-String heaterState;
-String coverState;
+String ledState = "OFF";
+String heaterState = "OFF";
+String coverState = "CLOSED";
+int pos = 180;  // variable to store the servo position
 
 // Timer bez delay-a varijable
 unsigned long previousMillis = 0;  // will store last time LED was updated
@@ -40,12 +51,7 @@ AsyncWebServer server(80);
 String processor(const String& var) {
   ////Serial.println(var);
   if (var == "STATE") {
-    if (digitalRead(ledENA)) {
-      ledState = "ON";
-    }
-    else {
-      ledState = "OFF";
-    }
+
     return ledState;
   }
 
@@ -81,6 +87,11 @@ String processor(const String& var) {
     return val;
   }
 
+  if (var == "iP") {
+    String val = getValue(spiffsRead("WL"), ';', 2);
+    return val;
+  }
+
   return String();
 }
 
@@ -89,13 +100,15 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
 
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
   myservo.attach(servoPin, 500, 2400);  // attaches the servo on pin 18 to the servo object
 
   pinMode(servoENA, OUTPUT);
   pinMode(ledENA, OUTPUT);
   pinMode(heaterENA, OUTPUT);
-  pinMode(ledENA, OUTPUT);
-
 
   // Initialize SPIFFS
   if (!SPIFFS.begin(true)) {
@@ -103,30 +116,7 @@ void setup() {
     return;
   }
 
-  // Ako se u 5 navrata ne uspije spojiti na WiFi, onda generira svoj AP gdje se mogu spojiti na web server
-  int i = 0;
-  String val = spiffsRead("WL");
-  Serial.println("SSID: " + getValue(val, ';', 0) + "  PASS:" + getValue(val, ';', 1));
-
-  WiFi.begin(string2char(getValue(val, ';', 0)), string2char(getValue(val, ';', 1)));
-  while (WiFi.status() != WL_CONNECTED && i <= 5) {
-    i++;
-    delay(1000);
-    Serial.println("Connecting to WiFi..");  // Print ESP32 Local IP Address
-  }
-
-  Serial.println(WiFi.localIP());
-  if (i > 5) {
-    // Connect to Wi-Fi network with SSID and password
-    Serial.print("Setting AP (Access Point)…");
-    // Remove the password parameter, if you want the AP (Access Point) to be open
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, password);
-
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
-  }
+  initWiFi();
 
   // Definiranje html datoteka spremljenih u spiffsima
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
@@ -146,7 +136,7 @@ void setup() {
 
       if (p->name() == "led") {
         var1 = p->value();
-        if (var1 == "1") ledON(255);
+        if (var1 == "1") ledON(ledMax);
         if (var1 == "0") ledON(0);
       }
       if (p->name() == "heater") {
@@ -192,7 +182,7 @@ void setup() {
 
       if (p->name() == "led") {
         var1 = p->value();
-        if (var1 == "0") ledON(255);
+        if (var1 == "0") ledON(ledMax);
         if (var1 == "1") ledON(0);
       }
     }
@@ -209,7 +199,7 @@ void setup() {
       if (p->name() == "heater") {
         var1 = p->value();
         if (var1 == "0") ledON(0); /// zamjeniti ovo s fjom gdje pali grijanje
-        if (var1 == "1") ledON(255);/// zamjeniti ovo s fjom gdje gasi grijanje
+        if (var1 == "1") ledON(ledMax);/// zamjeniti ovo s fjom gdje gasi grijanje
       }
     }
   });
@@ -224,8 +214,12 @@ void setup() {
 
       if (p->name() == "cover") {
         var1 = p->value();
-        if (var1 == "1") {} //openCover(); /// zamjeniti ovo s fjom gdje otvara cover
-        if (var1 == "0") {} //closeCover();/// zamjeniti ovo s fjom gdje zatvara cover
+        if (var1 == "1") {
+          openCover(); //openCover(); /// zamjeniti ovo s fjom gdje otvara cover
+        }
+        if (var1 == "0") {
+          closeCover(); //closeCover();/// zamjeniti ovo s fjom gdje zatvara cover
+        }
       }
     }
   });
@@ -234,9 +228,9 @@ void setup() {
 }
 
 void loop() {
-  thermistorValue1 = analogRead(thermistorPin1);
- Serial.println(thermistorValue1);
-  delay(500);
+  //thermistorValue1 = analogRead(thermistorPin1);
+  //Serial.println(thermistorValue1);
+  //delay(500);
 
   // thermistorValue2 = analogRead(thermistorPin2);
   // Serial.println(thermistorValue2);
@@ -326,22 +320,23 @@ String spiffsWrite (String filename, String podaci) { ////fja koja zapisuje poda
 
 void closeCover() {
   //digitalWrite(servoENA, LOW);
-  myservo.attach(servoPin);
-  if (coverState != "CLOSED") {    
-    for (pos = 0; pos <= 180; pos++) {  // goes from 0 degrees to 180 degrees9
+  myservo.attach(servoPin, 500, 2400);
+  if (coverState != "CLOSED") {
+    for (pos = 0; pos <= 180; pos++) {  // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo.write(pos);  // tell servo to go to position in variable 'pos'
       delay(15);           // waits 15ms for the servo to reach the position
     }
     Serial.print("#");
     //  digitalWrite(servoENA, HIGH);
-    coverState = "CLOSED";
-    myservo.detach();
+
   }
+  coverState = "CLOSED";
+  myservo.detach();
 }
 
 void openCover() {
-  myservo.attach(servoPin);
+  myservo.attach(servoPin, 500, 2400);
   // digitalWrite(servoENA, LOW);
   if (coverState != "OPEN") {
     for (pos = 180; pos >= 0; pos--) {  // goes from 180 degrees to 0 degrees
@@ -350,11 +345,49 @@ void openCover() {
     }
     Serial.print("#");
     // digitalWrite(servoENA, HIGH);
-    coverState = "OPEN";
-    myservo.detach();
   }
+  coverState = "OPEN";
+  myservo.detach();
 }
 
-void ledON (int P){
+void ledON (int P) {
+  analogWriteFrequency(ledENA, 1000); /// Reset freq change coused by ServoESP32 library
   analogWrite(ledENA, P);
+  if (P == 0) ledState = "OFF";
+  else ledState = "ON";
+}
+
+void initWiFi() {
+  // Ako se u 5 navrata ne uspije spojiti na WiFi, onda generira svoj AP gdje se mogu spojiti na web server
+  int i = 0;
+  String val = spiffsRead("WL");
+  Serial.println("SSID: " + getValue(val, ';', 0) + "  PASS:" + getValue(val, ';', 1));
+
+  WiFi.setHostname(hostname);
+
+  WiFi.begin(string2char(getValue(val, ';', 0)), string2char(getValue(val, ';', 1)));
+  while (WiFi.status() != WL_CONNECTED && i <= 5) {
+    i++;
+    delay(1000);
+    Serial.println("Connecting to WiFi..");  // Print ESP32 Local IP Address
   }
+
+  Serial.println(WiFi.localIP());
+  if (i > 5) {
+
+    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+      Serial.println("STA Failed to configure");
+    }
+
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Setting AP (Access Point)…");
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+  }
+}
